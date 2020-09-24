de0782c8b3a35137ce7fc700056731ed7b99344a
alskdj90 <wyong0122@naver.com>
2020-09-24 04:39:03 +0000

    test

---
+++ test/hello/target/rls/debug/deps/hello-f2c094e31ae3edfd.d
@@ -0,0 +1,5 @@
+/home/s20160301/cs492-concur/test/hello/target/rls/debug/deps/hello-f2c094e31ae3edfd.rmeta: src/main.rs
+
+/home/s20160301/cs492-concur/test/hello/target/rls/debug/deps/hello-f2c094e31ae3edfd.d: src/main.rs
+
+src/main.rs:
